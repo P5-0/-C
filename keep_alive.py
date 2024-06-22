206d53eda2f2147e1e7623582b668f86b0d7d6c0
𝑷𝑺𝒀𝑪𝑯𝑶𝑷𝑨𝑻𝑯 <165402942+P5-0@users.noreply.github.com>
2024-06-22 16:03:46 +0300

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+    # This returns a minimal HTML page directly
+    return '<!doctype html><html><head><title>Lightweight Page</title></head><body><p>Hello, World!</p></body></html>'
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
+
+if __name__ == "__main__":
+    keep_alive()
